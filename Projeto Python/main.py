0fa32a3212a90305d5b7e11d2bd18568c6c34e42
Leonardo Morais <94178073+leonardomoraisrp@users.noreply.github.com>
2022-01-20 09:20:21 -0300

    Trabalho base

---
+++ Projeto Python/main.py
@@ -0,0 +1,43 @@
+import os
+from turtle import color
+from fpdf import FPDF
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# GERANDO PDF
+pdf = FPDF('p', 'mm', 'A4')
+pdf.add_page()
+pdf.set_font('arial', '', 16)
+
+# ABRINDO PLANILHA ABAIXO
+dadosEstado = pd.read_excel(r'C:\Users\Note\Desktop\Projeto Python\Dados-covid-19-estado.xlsx')
+# print(dadosEstado)
+
+# PEGANDO INFORMAÇÕES DA COLUNA
+totalDeCasos = dadosEstado["Total de casos"]
+casosDia = dadosEstado["Casos por dia"]
+obitosDia = dadosEstado["Óbitos por dia"]
+
+
+plt.plot(totalDeCasos, '--')
+plt.xlabel('Total de casos')
+plt.savefig("exemplo.png")
+plt.close()
+
+pdf.multi_cell(w=0, h=8, txt="Grafico Total de Casos no estado de São Paulo", ln=1, align='C')
+pdf.image(x=20, y=30, w=180, h=80, name='exemplo.png')
+plt.plot(casosDia)
+plt.xlabel('Casos por dia')
+plt.savefig("exemplo2.png")
+plt.close()
+pdf.multi_cell(w=0, h=230, txt="Grafico de Casos por Dia no estado de São Paulo", ln=1, align='C')
+pdf.image(x=20, y=140, w=180, h=80, name='exemplo2.png')
+plt.plot(obitosDia)
+plt.xlabel('Obitos por dia')
+plt.savefig("exemplo3.png")
+plt.close()
+pdf.multi_cell(w=0, h=30, txt="Grafico Óbitos por Dia no estado de São Paulo", ln=1, align='C')
+pdf.image(x=20, y=50, w=180, h=80, name='exemplo3.png')
+pdf.output('relatorio.pdf')
+
+os.system("pause")
