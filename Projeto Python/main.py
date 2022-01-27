b042c73546e18ca13605d0cfc51c1e1705d8ca0e
Leonardo Morais <94178073+leonardomoraisrp@users.noreply.github.com>
2022-01-27 13:08:02 -0300

    Atualização

--- Projeto Python/main.py
+++ Projeto Python/main.py
@@ -1,5 +1,4 @@
 import os
-from turtle import color
 from fpdf import FPDF
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,23 +17,22 @@ totalDeCasos = dadosEstado["Total de casos"]
 casosDia = dadosEstado["Casos por dia"]
 obitosDia = dadosEstado["Óbitos por dia"]
 
-
 plt.plot(totalDeCasos, '--')
 plt.xlabel('Total de casos')
-plt.savefig("exemplo.png")
+plt.savefig("grafico_01.png")
 plt.close()
 
 pdf.multi_cell(w=0, h=8, txt="Grafico Total de Casos no estado de São Paulo", ln=1, align='C')
 pdf.image(x=20, y=30, w=180, h=80, name='exemplo.png')
 plt.plot(casosDia)
 plt.xlabel('Casos por dia')
-plt.savefig("exemplo2.png")
+plt.savefig("grafico_02.png")
 plt.close()
 pdf.multi_cell(w=0, h=230, txt="Grafico de Casos por Dia no estado de São Paulo", ln=1, align='C')
 pdf.image(x=20, y=140, w=180, h=80, name='exemplo2.png')
 plt.plot(obitosDia)
 plt.xlabel('Obitos por dia')
-plt.savefig("exemplo3.png")
+plt.savefig("grafico_03.png")
 plt.close()
 pdf.multi_cell(w=0, h=30, txt="Grafico Óbitos por Dia no estado de São Paulo", ln=1, align='C')
 pdf.image(x=20, y=50, w=180, h=80, name='exemplo3.png')
